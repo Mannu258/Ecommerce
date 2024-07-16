aa3477bc8cd0d9940da5b7dd6ba721d4a9c9622c
Your Name <you@example.com>
2024-07-16 09:47:34 +0530

    fixed template and static files

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["ecommerceapp/Templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 
 # Default primary key field type
