4fd63d9d48d28155a6cfc8f0f7b848787020fa69
Your Name <you@example.com>
2024-07-26 16:53:44 +0530

    frontend updating

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -3,14 +3,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Cateogery(models.Model):
     cat = models.CharField(max_length=30)
-
     def __str__(self) -> str:
         return self.cat
+    
+class subCateogery(models.Model):
+    cat = models.ForeignKey(Cateogery,on_delete=models.CASCADE)
+    subcat = models.CharField(max_length=30)
 
 class Product(models.Model):
-    cat = models.ForeignKey(Cateogery,on_delete=models.CASCADE)
+    cat = models.ForeignKey(subCateogery,on_delete=models.CASCADE)
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     price = models.IntegerField(default = 0)
